4ec04c341c094f2e62efd6bfeb3f5913823af80f
Saeed Alalwan <alalwan@gmail.com>
2018-06-26 21:28:14 -0400

    Comment out user endpoint again.

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,7 +1,7 @@
 import { version } from "../../package.json";
 import { Router } from "express";
 import login from "./login";
-import user from "./user";
+// import user from "./user";
 import admin from "./admin";
 import passport from "passport";
 
@@ -10,7 +10,7 @@ export default ({ config, db }) => {
 
   api.use("/login", login({ config, db }));
 
-  api.use("/user", user({ config, db }));
+  // api.use("/user", user({ config, db }));
 
   api.use(
     "/admin",
