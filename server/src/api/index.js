fd2fcd466a861f1fa640a6d9314bbd5e2c1dbf3a
Saeed Alalwan <alalwan@gmail.com>
2018-09-18 23:29:29 -0400

    test get survey without authentication

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,6 +1,7 @@
 import { version } from "../../package.json";
 import { Router } from "express";
 import login from "./login";
+import survey from "./survey";
 // import user from "./user";
 import admin from "./admin";
 import passport from "passport";
@@ -9,6 +10,8 @@ export default ({ config, db, emitter }) => {
   const api = Router();
 
   api.use("/login", login({ config, db }));
+
+  api.use("/test", survey({ config, db }));
 
   // api.use("/user", user({ config, db }));
 
