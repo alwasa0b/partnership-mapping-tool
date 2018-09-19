e274123ff5530ed325d06c590e2cc9e57233dff9
Saeed Alalwan <alalwan@gmail.com>
2018-09-18 23:04:37 -0400

    add Access-Control-Allow-Origin

--- server/src/index.js
+++ server/src/index.js
@@ -19,18 +19,14 @@ app.use(morgan("dev"));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin,X-Requested-With,Content-Type,x-auth,Accept,content-type,application/json"
   );
   next();
 });
-
-// app.use(
-//   cors({
-//     exposedHeaders: config.corsHeaders
-//   })
-// );
 
 app.use(passport.initialize());
 
