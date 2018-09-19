967dda7b8a21bb5ff8c4c64e1de226d49bf5a2e5
Saeed Alalwan <alalwan@gmail.com>
2018-09-18 23:56:27 -0400

    Remove that test route

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,7 +1,6 @@
 import { version } from "../../package.json";
 import { Router } from "express";
 import login from "./login";
-import survey from "./survey";
 // import user from "./user";
 import admin from "./admin";
 import passport from "passport";
@@ -10,8 +9,6 @@ export default ({ config, db, emitter }) => {
   const api = Router();
 
   api.use("/login", login({ config, db }));
-
-  api.use("/test", survey({ config, db }));
 
   // api.use("/user", user({ config, db }));
 
